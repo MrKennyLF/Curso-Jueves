335f31b9da3ea2ef3f998892a0e0d55f37f8b980
Danelly gonzalez <danigonzalez0906@gmail.com>
2025-10-16 17:39:59 -0600

    Se migró el html a angular con componentes navbar, footer, home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { Home } from './page/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:'home', component: Home},
+{ path:'', redirectTo: '/home', pathMatch: 'full'}]
